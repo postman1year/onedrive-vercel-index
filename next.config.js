1cb03d071c431b846751719be242dd8d19b36dab
postman1year <postman1year@gmail.com>
2026-02-19 00:45:20 +0800

    First Next16 test NO-i18n

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  reactStrictMode: true,
+  turbopack: {
+    // 示例：添加别名和自定义文件扩展名
+    resolveAlias: {
+      underscore: 'lodash',
+    },
+    resolveExtensions: ['.mdx', '.tsx', '.ts', '.jsx', '.js', '.json'],
+  },
 }
