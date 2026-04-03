7b6c3b411c0a52d8b3ef081eccb8600689da0143
postman1year <postman1year@gmail.com>
2026-04-03 18:19:21 +0800

    第一個Nextjs-v16的版本
    2026年03月18日升級至Nextjs-v16.2.2, 此為第一個Nextjs-v16的版本
     
    2026年03月31日升級TypeScript至v6版本
    
    
    一、上一個版本已把多語言套件Next-i18next剝離(大部分檔案有修改, 而且刪除了幾個檔案)
        原來項目一直存在的多語言套件Next-18next, 會導致NEXT js升級v16後在vercel部署後必然出現--錯誤500, 故已刪除,
    
    二、此版本為第一個將Next js 升級至v16的版本, 因已移除Next-i18next, 故已不再出現---錯誤500
    
    三、Node 仍為v24 版本, 及仍為Page Router項目
    
    四、Nextjs 升級至 v16, 原來一直使用的 Webpack 換成 Turbopack
      
    五、TypeScript升級至v6版本, 所以tsconfig.json內的"moduleResolution": 由 "node" 改為 "bundler"
    
    
    能在vercel成功部署後, 一切正常, 而成為最新底包
    
    
    未來, 可能先將page router 轉換為 app router(艱巨)後, 才會加入最先進的多語言套件(例如INTLAYER, NEXT-INTL等)

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
+// next.config.js
 module.exports = {
   reactStrictMode: true,
-}
+  turbopack: {
+    resolveAlias: {
+      // Alias 'underscore' package imports to 'lodash'
+      'underscore': 'lodash',
+      // Alias specific internal polyfill paths
+      'next/dist/build/polyfills/polyfill-module': './lib/modern-polyfill.js',
+    },
+	resolveExtensions: ['.mdx', '.tsx', '.ts', '.jsx', '.js', '.mjs', '.json'],
+  },
+};
